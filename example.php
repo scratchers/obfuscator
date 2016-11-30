32890da3cf45f156d337e1910a7ae70bd8614dd5
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-30 17:47:27 +0000

    fix notice with dummy data

--- example.php
+++ example.php
@@ -1,9 +1,13 @@
 <?php
 
 $example = 'some $string';
+
 function exampleFunction($variable2){
   echo $variable2;
 }
+
+$variable3 = array('example','another');
+
 foreach($variable3 as $key => $var3val){
   echo $var3val."somestring";
 }
