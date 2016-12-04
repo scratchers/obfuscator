a48142aa0dd59d5e761be3e55b224bb70d27d5df
Jeff Puckett <jeff@jeffpuckett.com>
2016-12-04 15:44:08 +0000

    add test

--- example.php
+++ example.php
@@ -7,13 +7,18 @@ if(isset($_POST['something'])){
 }
 
 function exampleFunction($variable2){
-  echo $variable2;
+  echo $variable2.PHP_EOL;
 }
 
 exampleFunction($example);
 
+$var = 'variable';
+$$var = 'changed variable';
+
+exampleFunction($variable);
+
 $variable3 = array('example','another');
 
 foreach($variable3 as $key => $var3val){
-  echo $var3val."somestring";
+  echo $var3val."somestring".PHP_EOL;
 }
